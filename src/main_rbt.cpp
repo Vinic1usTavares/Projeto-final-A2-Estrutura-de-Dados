3745003ce8f1bf51a3287226f66e42f0f4c5296d
Gabriel Schuenker <gabrielschuenker@outlook.com>
2025-06-12 14:06:33 -0300

    Adiciona os testes de funcionamento a main rbt

--- src/main_rbt.cpp
+++ src/main_rbt.cpp
@@ -1,22 +1,98 @@
 #include "rbt.h"
+#include "data.h"
+#include "test.h"
 #include "tree_utils.h"
 #include <iostream>
+#include <string>
+#include <chrono>
 
-int main() {
-    // Cria a árvore
+// int main() {
+//     // Cria a árvore
+//     BinaryTree* tree = RBT::create();
+
+//     // Insere palavras com IDs
+//     RBT::insert(tree, "banana", 1);
+//     RBT::insert(tree, "abacate", 1);
+//     RBT::insert(tree, "caju", 2);
+//     RBT::insert(tree, "damasco", 2);
+
+//     // Imprime a árvore
+//     printTree(tree);
+
+//     // Libera memória
+//     RBT::destroy(tree);
+
+//     return 0;
+// }
+
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Uso: ./bin/bst <search|stats> <n_docs> <diretorio>\n";
+        std::cerr << "     ./bin/bst tests   (roda os testes unitários)\n";
+        return 1;
+    }
+
+    std::string command = argv[1];
+
+    // Caso de testes
+    if(command == "tests") {
+        //run_rbt_tests();
+        return 0;
+    }
+
+    int n_docs = std::stoi(argv[2]);
+    std::string dir = argv[3];
+
     BinaryTree* tree = RBT::create();
 
-    // Insere palavras com IDs
-    RBT::insert(tree, "banana", 1);
-    RBT::insert(tree, "abacate", 1);
-    RBT::insert(tree, "caju", 2);
-    RBT::insert(tree, "damasco", 2);
+    auto start_index = std::chrono::high_resolution_clock::now();
+    auto documents = load_documents(dir, n_docs);
+    std::vector<InsertResult> insert_results = index_documents(tree, documents, RBT::insert);
+    auto end_index = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> index_time = end_index - start_index;
 
-    // Imprime a árvore
-    printTree(tree);
+    if (command == "search") {
+        std::string query;
+        bool running = true;
+        while (running) {
+            std::cout << "Digite --quit para abortar a execucao." << std::endl;
+            std::cout << "Digite uma palavra para buscar: ";
+            std::cin >> query;
+            if(query == "--quit") {
+                running = false;
+                break;
+            }
+            SearchResult result = RBT::search(tree, query);
+            if(result.found == 0) {
+                std::cout << query << " nao esta presente nos textos \n";
+            } else {
+                std::cout << "A palavra: \"" << query << "\" foi encontrada em " 
+                          << result.documentIds.size() << " documento(s): ";
+                for(size_t i = 0; i < result.documentIds.size(); ++i) {
+                    std::cout << result.documentIds[i];
+                    if(i < result.documentIds.size() - 1) std::cout << ',';
+                }
+                std::cout << ",\nTempo total da busca: " << result.executionTime << " ms \n";
+                std::cout << "Total de comparacoes feitas: " << result.numComparisons << "\n";
+                std::cout << std::endl;
+            }
+        }
+    } 
+    else if (command == "stats") {
+        int total_comparisons = 0;
+        for (const auto& res : insert_results) {
+            total_comparisons += res.numComparisons;
+        }
 
-    // Libera memória
+        std::cout << "\n=== Estatísticas RBT ===\n";
+        std::cout << "Documentos indexados: " << n_docs << "\n";
+        std::cout << "Tempo total de indexação: " << index_time.count() << " segundos\n";
+        std::cout << "Comparações totais (inserção): " << total_comparisons << "\n";
+        std::cout << "Altura da árvore: " << (tree->root ? tree->root->height : 0) << "\n\n";
+
+    }
+
     RBT::destroy(tree);
-
     return 0;
 }
--- src/rbt.cpp
+++ src/rbt.cpp
@@ -76,50 +76,52 @@ namespace RBT {
     }
 
     void fixInsertion(BinaryTree* tree, Node* node) {
-    while (node != tree->root && node->parent->isRed) {
-        Node* parent = node->parent;
-        Node* grandparent = parent->parent;
+        while (node != tree->root && node->parent->isRed) {
+            Node* parent = node->parent;
+            Node* grandparent = parent->parent;
 
-        if (parent == grandparent->left) {
-            Node* uncle = grandparent->right;
+            if (parent == grandparent->left) {
+                Node* uncle = grandparent->right;
 
-            if (uncle->isRed) {
-                parent->isRed = 0;
-                uncle->isRed = 0;
-                grandparent->isRed = 1;
-                node = grandparent;
-            } else {
-                if (node == parent->right) {
-                    node = parent;
-                    leftRotate(tree, node);
-                    parent = node->parent;
+                if (uncle->isRed) {
+                    parent->isRed = 0;
+                    uncle->isRed = 0;
+                    grandparent->isRed = 1;
+                    node = grandparent;
+                } else {
+                    if (node == parent->right) {
+                        node = parent;
+                        leftRotate(tree, node);
+                        parent = node->parent;
+                    }
+                    parent->isRed = 0;
+                    grandparent->isRed = 1;
+                    rightRotate(tree, grandparent);
                 }
-                parent->isRed = 0;
-                grandparent->isRed = 1;
-                rightRotate(tree, grandparent);
-            }
-        } else {
-            Node* uncle = grandparent->left;
+            } 
+            else {
+                Node* uncle = grandparent->left;
 
-            if (uncle->isRed) {
-                parent->isRed = 0;
-                uncle->isRed = 0;
-                grandparent->isRed = 1;
-                node = grandparent;
-            } else {
-                if (node == parent->left) {
-                    node = parent;
-                    rightRotate(tree, node);
-                    parent = node->parent;
+                if (uncle->isRed) {
+                    parent->isRed = 0;
+                    uncle->isRed = 0;
+                    grandparent->isRed = 1;
+                    node = grandparent;
+                } 
+                else {
+                    if (node == parent->left) {
+                        node = parent;
+                        rightRotate(tree, node);
+                        parent = node->parent;
+                    }
+                    parent->isRed = 0;
+                    grandparent->isRed = 1;
+                    leftRotate(tree, grandparent);
                 }
-                parent->isRed = 0;
-                grandparent->isRed = 1;
-                leftRotate(tree, grandparent);
             }
         }
+        tree->root->isRed = 0;
     }
-    tree->root->isRed = 0;
-}
     InsertResult insert(BinaryTree* tree, const std::string& word, int documentId) {
     auto start = std::chrono::high_resolution_clock::now();
     int comparisons = 0;
@@ -140,9 +142,11 @@ namespace RBT {
         comparisons++;
         if (word < current->word) {
             current = current->left;
-        } else if (word > current->word) {
+        } 
+        else if (word > current->word) {
             current = current->right;
-        } else {
+        } 
+        else {
             // Palavra já existe, só adiciona docId se não for repetido
             if (current->documentIds.empty() || current->documentIds.back() != documentId)
                 current->documentIds.push_back(documentId);
@@ -191,7 +195,8 @@ namespace RBT {
             
             if(word < node->word) {
                 node = node->left;
-            } else {
+            } 
+            else {
                 node = node->right;
             }
         }
@@ -206,14 +211,14 @@ namespace RBT {
     }
 
     void destroyNode(Node* node, Node* NIL) {
-    if (node == nullptr || node == NIL) return;
+    if (node == NIL) { return; }
     destroyNode(node->left, NIL);
     destroyNode(node->right, NIL);
     delete node;
 }
 
     void destroy(BinaryTree* tree) {
-        if (tree == nullptr) return;
+        if (tree == nullptr) { return; }
         destroyNode(tree->root, tree->NIL);
         delete tree;
     }
