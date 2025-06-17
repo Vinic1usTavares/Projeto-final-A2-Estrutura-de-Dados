b5539d3362999b25b32712e452c401fc3756c2b9
adriel <adriel25dias@gmail.com>
2025-06-17 20:14:06 -0300

    deleted preliminary tests

--- src/main_rbt.cpp
+++ src/main_rbt.cpp
@@ -6,24 +6,6 @@
 #include <string>
 #include <chrono>
 
-// int main() {
-//     // Cria a árvore
-//     BinaryTree* tree = RBT::create();
-
-//     // Insere palavras com IDs
-//     RBT::insert(tree, "banana", 1);
-//     RBT::insert(tree, "abacate", 1);
-//     RBT::insert(tree, "caju", 2);
-//     RBT::insert(tree, "damasco", 2);
-
-//     // Imprime a árvore
-//     printTree(tree);
-
-//     // Libera memória
-//     RBT::destroy(tree);
-
-//     return 0;
-// }
 
 
 int main(int argc, char* argv[]) {
